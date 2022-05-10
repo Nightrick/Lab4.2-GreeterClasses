02efe7725ac7198304ac0d772f62c80806426a22
David <brushdm@gmail.com>
2022-05-09 21:51:15 -0400

    Second commit for Lab 4.2. More work needed on extended challenges.

---
+++ GreeterClasses/src/LoudGreeter.java
@@ -0,0 +1,19 @@
+
+public class LoudGreeter extends Greeter {
+	
+	private String extra = "!!";
+
+	public LoudGreeter(String greeting) {
+		super(greeting);
+	}
+	
+	public void addVolume() {
+		extra += "!";
+	}
+	
+	@Override
+	public String greet(String name) {
+		return greeting + ", " + name + extra;
+	}
+
+}
